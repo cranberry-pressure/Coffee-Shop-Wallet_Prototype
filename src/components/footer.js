581fce5e673b58d532d34fe6b91d931d3fc6c5ea
Jimmy Debe <91767824+cranberry-pressure@users.noreply.github.com>
2022-10-22 23:10:41 -0400

    Create footer.js

---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import '../App.css';
+
+class Footer extends Component{
+  render(){
+    return(
+      <div>
+        <div className= "footer">
+        
+        <h3>Made with Love :)</h3>
+        
+        </div>
+      </div>
+
+    )
+  }
+}
+
+export default Footer;
