1c426d05400625407ac5ad1c58afc93ce14a9ad7
Jimmy Debe <91767824+cranberry-pressure@users.noreply.github.com>
2022-10-24 18:26:21 -0400

    Create menu.js

---
+++ src/pages/menu.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class Menu extends Component {
+  render(){
+    return(
+       <div className="App-header">
+          <h1>Menu items</h1>
+          <img src={"https://img.freepik.com/free-vector/coffee-love-foam-with-beans-cartoon-icon-illustration_138676-2575.jpg"}  />
+
+       </div>
+    );
+
+  }
+}
+
+export default Menu;
